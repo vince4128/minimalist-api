4fc768e906662103ed7f060c7fcb2a74d5955571
vince4128 <vrlt.vincent@gmail.com>
2018-09-10 17:51:43 +0200

    authorization headers allowed (cors)

--- server/server.config.js
+++ server/server.config.js
@@ -26,7 +26,7 @@ module.exports = {
 
             //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3003');
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization');
 
             // Set to true if you need the website to include cookies in the requests sent
             // to the API (e.g. in case you use sessions)
