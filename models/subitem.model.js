00242e08c8125dc70c7b699e8703b1a54009b3ae
vince4128 <vrlt.vincent@gmail.com>
2018-09-13 18:02:40 +0200

    string type changed to ObjectId

--- models/subitem.model.js
+++ models/subitem.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const subitemSchema = new Schema({
     title:String,
     text:String,
-    image:String
+    image:{type: mongoose.Schema.Types.ObjectId, ref: 'Image'}
 });
 
 const Subitem = mongoose.model("Subitem", subitemSchema);
