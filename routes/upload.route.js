9558933b547bcd2204941a9a171b81bd3bb2e9a5
vince4128 <vrlt.vincent@gmail.com>
2018-09-19 17:41:16 +0200

    auth added

--- routes/upload.route.js
+++ routes/upload.route.js
@@ -5,28 +5,33 @@ const   express = require("express"),
         path    = require('path'),
         multer  = require("multer");     
 
-//const Authentication = require('../controllers/authentication');
-//const passportService = require('../services/passport');
-//const passport = require('passport');
+const Authentication = require('../controllers/authentication');
+const passportService = require('../services/passport');
+const passport = require('passport');
                 
-//const requireAuth = passport.authenticate('jwt', { session: false });
-//const requireSignin = passport.authenticate('local', { session: false });
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 const storage = multer.diskStorage({    
     destination: './upload/',
     filename: function (req, file, cb) {
-      crypto.pseudoRandomBytes(16, function (err, raw) {
-        if (err) return cb(err)
-  
-        cb(null, raw.toString('hex') + path.extname(file.originalname))
-      })
+      if(req.body.filename){
+        //cb(null, req.body.filename + '-' + Date.now() + path.extname(file.originalname))
+        cb(null, req.body.filename + '-' + Date.now() + path.extname(file.originalname))
+      }else{
+        crypto.pseudoRandomBytes(16, function (err, raw) {
+          if (err) return cb(err)
+    
+          cb(null, raw.toString('hex') + path.extname(file.originalname))
+        })
+      }
     }
   })
 
 upload = multer({storage});
 
 //Create add a new upload
-router.post("/", upload.single('file'), (req,res)=>{
+router.post("/", requireAuth, upload.single('file'), (req,res)=>{
 
     //fonctionne
     console.log('upload route', req.body, req.file, req.file.originalname);
